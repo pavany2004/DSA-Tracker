b620104ea7ae1915137c8a9d34f20a504868edd2
Pavan Kumar Yadav <140616621+pavany2004@users.noreply.github.com>
2024-07-20 11:46:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ export default function App() {
     return ( 
         <> 
             <div className="flex flex-col justify-center items-center mt-4"> 
-                <h1 className="text-emerald-500 font-bold text-3xl"> 
-                    GeeksforGeeks 
-                </h1> 
                 <h3 className="bg-clip-text text-transparent bg-gradient-to-r  
                                from-purple-500 to-pink-500 font-bold text-xl mb-4"> 
                     DSA Tracker 
