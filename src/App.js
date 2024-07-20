a488bd2a46a85518976abcc8ce07b37021382739
Pavan Kumar Yadav <140616621+pavany2004@users.noreply.github.com>
2024-07-20 10:04:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import DSAList from "./components/DSAList"; 
+  
+export default function App() { 
+    return ( 
+        <> 
+            <div className="flex flex-col justify-center items-center mt-4"> 
+                <h1 className="text-emerald-500 font-bold text-3xl"> 
+                    GeeksforGeeks 
+                </h1> 
+                <h3 className="bg-clip-text text-transparent bg-gradient-to-r  
+                               from-purple-500 to-pink-500 font-bold text-xl mb-4"> 
+                    DSA Tracker 
+                </h3> 
+                <DSAList /> 
+            </div> 
+        </> 
+    ); 
+}
